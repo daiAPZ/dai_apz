a7bf2dd67f0ed94c079fb8856ea6ce7b23b5a788
diaz-11 <angiediazvilca12@gmail.com>
2023-08-16 11:59:09 -0300

    Blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -42,5 +42,14 @@ def create_app(test_config=None):
 
     return app
 
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
 
 
